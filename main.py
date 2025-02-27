63f3e4398db3e039f741663e31846a3e488a07c3
Arvind Kharwal <karvind629@gmail.com>
2025-02-27 16:14:55 +0530

    27 February 2025

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import streamlit as st
 st.title('Introduction to Streamlit ')
 st.text('Hello, streamlit')
+st.text('Hello, streamlit',help='This is text')
+code = ''' def fun():
+	print('This is function') '''
+
+st.code(code)
